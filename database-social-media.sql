474d59833c9fda67b71e27480250a8a922f4ea3c
Ali Azmoodeh <treeroot.ir@gmail.com>
2022-04-19 00:31:23 +0430

    fix bug and add ON DELETE , ON UPDATE

--- database-social-media.sql
+++ database-social-media.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 17, 2022 at 08:56 PM
+-- Generation Time: Apr 18, 2022 at 09:59 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.3.31
 
@@ -41,17 +41,6 @@ CREATE TABLE `090307892andazi1578cat_e2e_data` (
   `file_name` varchar(12) DEFAULT NULL,
   `file_size` varchar(10) DEFAULT NULL,
   `is_forward` varchar(5) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `090307892andazi1578cat_reply`
---
-
-CREATE TABLE `090307892andazi1578cat_reply` (
-  `message_target_id` int(11) NOT NULL,
-  `message_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -107,25 +96,13 @@ CREATE TABLE `channel_joins` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `channel_reply`
---
-
-CREATE TABLE `channel_reply` (
-  `message_target_id` int(11) NOT NULL,
-  `message_id` int(11) DEFAULT NULL,
-  `channel_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `const_message_type`
 --
 
 CREATE TABLE `const_message_type` (
   `type` varchar(8) DEFAULT NULL,
-  `id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='''image'',''location'',''document'',''video'',''voice''';
+  `id` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Types -> ''image'',''location'',''document'',''video'',''voice''';
 
 -- --------------------------------------------------------
 
@@ -134,9 +111,9 @@ CREATE TABLE `const_message_type` (
 --
 
 CREATE TABLE `const_report_type` (
-  `id` int(11) NOT NULL,
+  `id` smallint(6) NOT NULL,
   `type` varchar(16) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='''spam'',''fake account'',''violence'',''child abuse'',''illegal drugs'',''personal details'',''pornography'',''other''';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Types -> ''spam'',''fake account'',''violence'',''child abuse'',''illegal drugs'',''personal details'',''pornography'',''other''';
 
 -- --------------------------------------------------------
 
@@ -183,17 +160,6 @@ CREATE TABLE `googleandroid_group_data` (
   `file_name` varchar(12) DEFAULT NULL,
   `file_size` varchar(10) DEFAULT NULL,
   `is_forward` varchar(5) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `googleandroid_group_reply`
---
-
-CREATE TABLE `googleandroid_group_reply` (
-  `message_target_id` int(11) NOT NULL,
-  `message_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -266,7 +232,8 @@ CREATE TABLE `report_channel` (
   `channel_id` int(11) DEFAULT NULL,
   `type_id` smallint(6) DEFAULT NULL,
   `notes` varchar(255) DEFAULT NULL,
-  `status` enum('accept','pending','decline') DEFAULT NULL
+  `status` enum('accept','pending','decline') DEFAULT NULL,
+  `accept_at` varchar(16) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -280,22 +247,8 @@ CREATE TABLE `report_group` (
   `group_id` int(11) DEFAULT NULL,
   `type_id` smallint(6) DEFAULT NULL,
   `notes` varchar(250) DEFAULT NULL,
-  `status` enum('accept','pending','decline') DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `report_group_message`
---
-
-CREATE TABLE `report_group_message` (
-  `user_id` int(11) DEFAULT NULL,
-  `group_id` int(11) DEFAULT NULL,
-  `type` enum('spam','fake account','violence','child abuse','illegal drugs','personal details','pornography','other') DEFAULT NULL,
-  `notes` varchar(500) DEFAULT NULL,
   `status` enum('accept','pending','decline') DEFAULT NULL,
-  `message_id` int(11) DEFAULT NULL
+  `accept_at` varchar(16) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -321,34 +274,23 @@ CREATE TABLE `treer00t_personal_chat_data` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `treer00t_reply`
---
-
-CREATE TABLE `treer00t_reply` (
-  `message_target_id` int(11) NOT NULL,
-  `message_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
-  `phone` int(11) NOT NULL,
+  `phone` varchar(255) NOT NULL,
   `authCode` smallint(6) DEFAULT NULL,
   `bio` varchar(70) DEFAULT NULL,
   `username` varchar(32) DEFAULT NULL,
   `img` varchar(130) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `password` varchar(70) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
   `first_name` varchar(20) DEFAULT NULL,
   `last_name` varchar(20) DEFAULT NULL,
   `is_active` varchar(5) DEFAULT NULL,
   `is_blocked` varchar(5) DEFAULT NULL,
   `id` int(11) NOT NULL,
-  `api_key` varchar(50) NOT NULL
+  `api_key` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -369,7 +311,7 @@ CREATE TABLE `user_channel_chat_list` (
 --
 
 CREATE TABLE `user_e2e_chat_list` (
-  `chat_tbl_id` varchar(3000) DEFAULT NULL,
+  `chat_tbl_id` varchar(100) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -392,45 +334,41 @@ CREATE TABLE `user_group_chat_list` (
 -- Indexes for table `090307892andazi1578cat_e2e_data`
 --
 ALTER TABLE `090307892andazi1578cat_e2e_data`
-  ADD PRIMARY KEY (`sender_id`,`id`),
-  ADD KEY `text` (`text`(768)),
-  ADD KEY `receiver_id` (`receiver_id`),
-  ADD KEY `forward_data_id` (`forward_data_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `090307892ANDAZI1578Cat_E2e_Data_Text_Msg` (`text`(768)),
+  ADD KEY `senderId1` (`sender_id`),
+  ADD KEY `messageTypeId2` (`message_type_id`),
+  ADD KEY `receiverId3` (`receiver_id`),
+  ADD KEY `forwardDataId4` (`forward_data_id`);
 
 --
 -- Indexes for table `block_list`
 --
 ALTER TABLE `block_list`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `userTargetId6` (`user_target_id`);
 
 --
 -- Indexes for table `channels`
 --
 ALTER TABLE `channels`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `name` (`name`);
+  ADD KEY `Channels_Name` (`name`);
 
 --
 -- Indexes for table `channel_admin`
 --
 ALTER TABLE `channel_admin`
-  ADD KEY `channel_id` (`channel_id`),
-  ADD KEY `admin_id` (`admin_id`);
+  ADD KEY `adminId7` (`admin_id`),
+  ADD KEY `channelId8` (`channel_id`);
 
 --
 -- Indexes for table `channel_joins`
 --
 ALTER TABLE `channel_joins`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `channel_id` (`channel_id`);
-
---
--- Indexes for table `channel_reply`
---
-ALTER TABLE `channel_reply`
-  ADD KEY `message_target_id_copy_1` (`message_target_id`),
-  ADD KEY `message_id_copy_1` (`message_id`),
-  ADD KEY `channel_id_copy_3` (`channel_id`);
+  ADD KEY `userId9` (`user_id`),
+  ADD KEY `channelId10` (`channel_id`);
 
 --
 -- Indexes for table `const_message_type`
@@ -449,7 +387,7 @@ ALTER TABLE `const_report_type`
 --
 ALTER TABLE `devices`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD KEY `userId11` (`user_id`);
 
 --
 -- Indexes for table `forward_data`
@@ -462,85 +400,91 @@ ALTER TABLE `forward_data`
 --
 ALTER TABLE `googleandroid_group_data`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `text` (`text`(768));
-
---
--- Indexes for table `googleandroid_group_reply`
---
-ALTER TABLE `googleandroid_group_reply`
-  ADD KEY `message_target_id_copy_3` (`message_target_id`),
-  ADD KEY `message_id_copy_3` (`message_id`);
+  ADD KEY `GoogleAndroid_Group_Data_Text_Msg` (`text`(768)),
+  ADD KEY `messageTypeId12` (`message_type_id`),
+  ADD KEY `forwardDataId13` (`forward_data_id`);
 
 --
 -- Indexes for table `groups`
 --
 ALTER TABLE `groups`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `name` (`name`);
+  ADD KEY `Groups_Name` (`name`);
 
 --
 -- Indexes for table `group_admin`
 --
 ALTER TABLE `group_admin`
-  ADD KEY `group_id` (`group_id`);
+  ADD KEY `adminId14` (`admin_id`),
+  ADD KEY `groupId15` (`group_id`);
 
 --
 -- Indexes for table `group_joins`
 --
 ALTER TABLE `group_joins`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `group_id` (`group_id`);
+  ADD KEY `userId16` (`user_id`),
+  ADD KEY `groupId17` (`group_id`);
 
 --
 -- Indexes for table `mohsenpack_channel_data`
 --
 ALTER TABLE `mohsenpack_channel_data`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `text` (`text`(768));
+  ADD KEY `MohsenPack_Channel_Data_Text_Msg` (`text`(768)),
+  ADD KEY `messageTypeId` (`message_type_id`),
+  ADD KEY `forwardDataId` (`forward_data_id`);
 
 --
 -- Indexes for table `report_channel`
 --
 ALTER TABLE `report_channel`
-  ADD KEY `channel_id` (`channel_id`);
+  ADD KEY `userId18` (`user_id`),
+  ADD KEY `channelId19` (`channel_id`),
+  ADD KEY `typeId20` (`type_id`);
 
 --
 -- Indexes for table `report_group`
 --
 ALTER TABLE `report_group`
-  ADD KEY `group_id` (`group_id`);
+  ADD KEY `userId21` (`user_id`),
+  ADD KEY `groupId22` (`group_id`),
+  ADD KEY `typeId23` (`type_id`);
 
 --
 -- Indexes for table `treer00t_personal_chat_data`
 --
 ALTER TABLE `treer00t_personal_chat_data`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `text` (`text`(768));
+  ADD KEY `TREER00T_Personal_Chat_Data_Text_Msg` (`text`(768)),
+  ADD KEY `messageTypeId25` (`message_type_id`),
+  ADD KEY `forwardDataId26` (`forward_data_id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `phone` (`phone`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user_channel_chat_list`
 --
 ALTER TABLE `user_channel_chat_list`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `channelId27` (`group_id`);
 
 --
 -- Indexes for table `user_e2e_chat_list`
 --
 ALTER TABLE `user_e2e_chat_list`
-  ADD KEY `user_id` (`user_id`);
+  ADD KEY `userId29` (`user_id`);
 
 --
 -- Indexes for table `user_group_chat_list`
 --
 ALTER TABLE `user_group_chat_list`
-  ADD KEY `user_id` (`user_id`);
+  ADD KEY `userId30` (`user_id`),
+  ADD KEY `groupId31` (`group_id`);
 
 --
 -- Constraints for dumped tables
@@ -550,37 +494,108 @@ ALTER TABLE `user_group_chat_list`
 -- Constraints for table `090307892andazi1578cat_e2e_data`
 --
 ALTER TABLE `090307892andazi1578cat_e2e_data`
-  ADD CONSTRAINT `forward_data_id` FOREIGN KEY (`forward_data_id`) REFERENCES `forward_data` (`id`),
-  ADD CONSTRAINT `receiver_id` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `sender_id` FOREIGN KEY (`sender_id`) REFERENCES `users` (`phone`);
+  ADD CONSTRAINT `forwardDataId4` FOREIGN KEY (`forward_data_id`) REFERENCES `forward_data` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `messageTypeId2` FOREIGN KEY (`message_type_id`) REFERENCES `const_message_type` (`id`),
+  ADD CONSTRAINT `receiverId3` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `senderId1` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `block_list`
 --
 ALTER TABLE `block_list`
-  ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`phone`);
+  ADD CONSTRAINT `userId5` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `userTargetId6` FOREIGN KEY (`user_target_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `channel_admin`
 --
 ALTER TABLE `channel_admin`
-  ADD CONSTRAINT `admin_id` FOREIGN KEY (`admin_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `channel_id` FOREIGN KEY (`channel_id`) REFERENCES `channels` (`id`);
+  ADD CONSTRAINT `adminId7` FOREIGN KEY (`admin_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `channelId8` FOREIGN KEY (`channel_id`) REFERENCES `channels` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `channel_reply`
+-- Constraints for table `channel_joins`
 --
-ALTER TABLE `channel_reply`
-  ADD CONSTRAINT `channel_id_copy_3` FOREIGN KEY (`channel_id`) REFERENCES `channels` (`id`),
-  ADD CONSTRAINT `message_id_copy_1` FOREIGN KEY (`message_id`) REFERENCES `mohsenpack_channel_data` (`id`),
-  ADD CONSTRAINT `message_target_id_copy_1` FOREIGN KEY (`message_target_id`) REFERENCES `mohsenpack_channel_data` (`id`);
+ALTER TABLE `channel_joins`
+  ADD CONSTRAINT `channelId10` FOREIGN KEY (`channel_id`) REFERENCES `channels` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `userId9` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `googleandroid_group_reply`
+-- Constraints for table `devices`
 --
-ALTER TABLE `googleandroid_group_reply`
-  ADD CONSTRAINT `message_id_copy_3` FOREIGN KEY (`message_id`) REFERENCES `googleandroid_group_data` (`id`),
-  ADD CONSTRAINT `message_target_id_copy_3` FOREIGN KEY (`message_target_id`) REFERENCES `googleandroid_group_data` (`id`);
+ALTER TABLE `devices`
+  ADD CONSTRAINT `userId11` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `googleandroid_group_data`
+--
+ALTER TABLE `googleandroid_group_data`
+  ADD CONSTRAINT `forwardDataId13` FOREIGN KEY (`forward_data_id`) REFERENCES `forward_data` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `messageTypeId12` FOREIGN KEY (`message_type_id`) REFERENCES `const_message_type` (`id`);
+
+--
+-- Constraints for table `group_admin`
+--
+ALTER TABLE `group_admin`
+  ADD CONSTRAINT `adminId14` FOREIGN KEY (`admin_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `groupId15` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `group_joins`
+--
+ALTER TABLE `group_joins`
+  ADD CONSTRAINT `groupId17` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `userId16` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `mohsenpack_channel_data`
+--
+ALTER TABLE `mohsenpack_channel_data`
+  ADD CONSTRAINT `forwardDataId` FOREIGN KEY (`forward_data_id`) REFERENCES `forward_data` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `messageTypeId` FOREIGN KEY (`message_type_id`) REFERENCES `const_message_type` (`id`);
+
+--
+-- Constraints for table `report_channel`
+--
+ALTER TABLE `report_channel`
+  ADD CONSTRAINT `channelId19` FOREIGN KEY (`channel_id`) REFERENCES `channels` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `typeId20` FOREIGN KEY (`type_id`) REFERENCES `const_report_type` (`id`),
+  ADD CONSTRAINT `userId18` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `report_group`
+--
+ALTER TABLE `report_group`
+  ADD CONSTRAINT `groupId22` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `typeId23` FOREIGN KEY (`type_id`) REFERENCES `const_report_type` (`id`),
+  ADD CONSTRAINT `userId21` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `treer00t_personal_chat_data`
+--
+ALTER TABLE `treer00t_personal_chat_data`
+  ADD CONSTRAINT `forwardDataId26` FOREIGN KEY (`forward_data_id`) REFERENCES `forward_data` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `messageTypeId25` FOREIGN KEY (`message_type_id`) REFERENCES `const_message_type` (`id`);
+
+--
+-- Constraints for table `user_channel_chat_list`
+--
+ALTER TABLE `user_channel_chat_list`
+  ADD CONSTRAINT `channelId27` FOREIGN KEY (`group_id`) REFERENCES `channels` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `userId28` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user_e2e_chat_list`
+--
+ALTER TABLE `user_e2e_chat_list`
+  ADD CONSTRAINT `userId29` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user_group_chat_list`
+--
+ALTER TABLE `user_group_chat_list`
+  ADD CONSTRAINT `groupId31` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `userId30` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
